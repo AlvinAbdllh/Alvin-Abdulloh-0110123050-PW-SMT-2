f170b99b164082dccd5f406777c7039edbc49379
Alvin <146794628+AlvinAbdllh@users.noreply.github.com>
2024-03-06 09:22:05 +0700

    Add files via upload

---
+++ Praktikum 1/variable_user.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Alvin Abdulloh";
+$umur = 20;
+$berat = 50.5;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+echo "<br/>Hello $nama Apakabar";
+?>
