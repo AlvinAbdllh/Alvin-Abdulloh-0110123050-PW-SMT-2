c04de9978c76f435b1a2cf0e22a944afcf9825ce
Alvin <146794628+AlvinAbdllh@users.noreply.github.com>
2024-03-30 20:47:22 +0700

    Add files via upload

---
+++ Pertemuan-3/opsi_registrasi.php
@@ -0,0 +1,7 @@
+
+<?php
+$ar_prodi = ["SI" => "Sistem Informasi", "TI" => "Teknik Informatika", "BD" => "Bisnis Digital"];
+$ar_skill = ["HTML" => 10, "CSS" => 10, "JavaScript" => 20, "RWD Bootstrap" => 20, "PHP" => 30, "Python" => 30, "Java" => 50];
+$ar_domisili = [ "JKT" => "Jakarta","DPK" => "Depok","BGR" => "Bogor","TGR" => "Tanggerang","BKS" => "Bekasi","DLL" => "Lainnya",];
+
+?>
