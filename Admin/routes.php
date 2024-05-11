49d4d578494fd32ce656f4d0896260fe7b6cb92e
Alvin <146794628+AlvinAbdllh@users.noreply.github.com>
2024-05-11 11:09:59 +0700

    Delete Admin directory

--- Admin/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ar_menu =[
-    'Home' => 'index.php',
-    'Daftar' => 'daftar.php',
-    'Array Buah' => 'array_buah.php',
-    'Belanja' => 'form_belanja.php',
-    'Siswa Nilai' => 'form_nilai.php'
-];
