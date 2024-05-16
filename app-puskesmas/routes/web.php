c1dc7aec80655bad5b0c18938fe80adb2de854eb
Alvin <alvinabdulloh1140@gmail.com>
2024-05-16 23:17:19 +0700

    laravel

---
+++ app-puskesmas/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/salam', function () {
+    return "Selamat Datang di web Puskesmas";
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'show']);
+Route::get('/dashboard/laporan', [DashboardController::class, 'laporan']);
